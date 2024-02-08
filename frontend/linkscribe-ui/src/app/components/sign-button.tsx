5937ebf675de17314836fab71f4e1ec4ce46faf7
alejandropr05 <alejandropr05@hotmail.com>
2024-02-08 17:35:56 -0500

    Add sign-button.tsx

---
+++ frontend/linkscribe-ui/src/app/components/sign-button.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+export function SignButton () {
+  return (
+    <button
+      type="button"
+      className="text-white bg-[#00152a] font-medium rounded-full text-sm font-sans px-4 py-2"
+    >
+      Sign in
+    </button>
+  )
+} 
