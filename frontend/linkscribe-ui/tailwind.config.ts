3c558850c341156f6d470c0b65e5ea432aeccb09
alejandropr05 <alejandropr05@hotmail.com>
2024-02-08 17:32:35 -0500

    Add animation progress config

--- frontend/linkscribe-ui/tailwind.config.ts
+++ frontend/linkscribe-ui/tailwind.config.ts
@@ -13,6 +13,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        archivo: "'Archivo', sans-serif",
+        jakarta: "'Plus Jakarta Sans', sans-serif"
+      },
+      animation: {
+        progress: 'progress 1s infinite linear',
+      },
+      keyframes: {
+        progress: {
+          '0%': { transform: ' translateX(0) scaleX(0)' },
+          '40%': { transform: 'translateX(0) scaleX(0.4)' },
+          '100%': { transform: 'translateX(100%) scaleX(0.5)' },
+        },
+      },
+      transformOrigin: {
+        'left-right': '0% 50%',
+      }
     },
   },
   plugins: [],
