018f4ca987cdedb2ad66a93f0884fdffe9b49e78
alejandropr05 <alejandropr05@hotmail.com>
2024-02-09 17:01:56 -0500

    Refactor api.py

--- backend/src/app/api.py
+++ backend/src/app/api.py
@@ -2,30 +2,30 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
-from sqlapp import models
+from sqlapp import models, database
 from utils import constants
 from sqlapp.database import engine
 from app.bookmark_controller import router as bookmark_router
 from app.users_controller import router as users_router
-from model.model_loader import ModelLoader
-from model.scrap import ScrapTool
+from model.page_categorizer import PageCategorizer
+from model.scrap_tool import ScrapTool
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    app.state.model = ModelLoader(
+    app.state.categorizer = PageCategorizer(
         constants.ModelsPath.MODEL, constants.ModelsPath.VECTORIZER
     )
-    app.state.scrap = ScrapTool()
+    app.state.scrap_tool = ScrapTool()
     yield
 
     # Clean up the models and release the resources
-    del app.state.model
-    del app.state.scrap
+    del app.state.categorizer
+    del app.state.scrap_tool
 
 
 app = FastAPI(lifespan=lifespan)
-models.Base.metadata.create_all(bind=engine)
+database.Base.metadata.create_all(bind=engine)
 
 
 app.add_middleware(
@@ -37,10 +37,5 @@ app.add_middleware(
 )
 
 
-app.include_router(users_router, tags=["users"], prefix="/users")
+# app.include_router(users_router, tags=["users"], prefix="/users")
 app.include_router(bookmark_router, tags=["bookmark"], prefix="/bookmark")
-
-
-@app.get("/")
-async def home():
-    return {"message": f"{app.state=}"}
