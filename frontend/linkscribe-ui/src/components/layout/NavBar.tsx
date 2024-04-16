aab2c6af07fdd1ae2f8b0f99f633424b2c9fc044
alejandropr05 <alejandropr05@hotmail.com>
2024-04-16 15:55:05 -0500

    Refactor navigation links to support multiple active paths

--- frontend/linkscribe-ui/src/components/layout/NavBar.tsx
+++ frontend/linkscribe-ui/src/components/layout/NavBar.tsx
@@ -8,23 +8,20 @@ import OuterLink from "@public/outer-link.svg"
 export default function NavBar () {
   const pathname = usePathname()
   const navLinks = [
-    {name: "Create", href: "/"},
-    {name: "Saved", href: "/saved"}
+    {name: "Create", href: ["/", "/with-modal"]},
+    {name: "Saved", href: ["/saved"]}
   ]
   return (
     <div className="flex flex-row justify-center items-center space-x-5 font-medium font-sans md:text-base h-full">
-      {navLinks.map((link) => {
-        const isActive = pathname === link.href
-        return (
-          <a
-            href={link.href}
-            key={link.name}
-            className={`flex ${isActive ? "text-[#00152a]" : "text-[#60606b]"}`}
-          >
-            {link.name}
-          </a>
-        )
-      })}
+      {navLinks.map((link) =>
+        <a
+          href={link.href[0]}
+          key={link.name}
+          className={`flex ${link.href.includes(pathname ?? "") ? "text-[#00152a]" : "text-[#60606b]"}`}
+        >
+          {link.name}
+        </a>
+      )}
       <a className="text-[#60606b] hidden sm:flex items-center" href="https://github.com/alejandropr5/linkscribe" target="_blank">
         Source Code
         <div className="w-5 h-5 ml-1">
