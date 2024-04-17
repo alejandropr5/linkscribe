2c3d88ae968d26af63cff96a4cf0c4656a32ffd5
alejandropr05 <alejandropr05@hotmail.com>
2024-04-17 12:19:53 -0500

    Update API paths

--- frontend/linkscribe-ui/src/lib/constants.tsx
+++ frontend/linkscribe-ui/src/lib/constants.tsx
@@ -6,21 +6,21 @@ import defaultImage from "@public/default-img.png"
 */
 export const APIConstants = Object.freeze({
   PREDICT_PATH: "/bookmarks/predict",
-  CREATE_USER: "/users",
+  CREATE_USER: "/users/",
   LOGIN: "/users/login",
   GET_AVAILABILITY: "/users/available/",
   READ_USER_CATEGORY_ROOT: "/categories/root",
-  CREATE_USER_CATEGORY: "/categories",
+  CREATE_USER_CATEGORY: "/categories/",
   PATCH_USER_CATEGORY: "/categories/",
   DELETE_USER_CATEGORY: "/categories/",
-  READ_USER_BOOKMARKS: "/bookmarks",
+  READ_USER_BOOKMARKS: "/bookmarks/",
   PATCH_USER_BOOKMARK: "/bookmarks/",
   DELETE_USER_BOOKMARK: "/bookmarks/"
 })
 
 export const pathNames = Object.freeze({
-  login: "/auth/login",
-  signUp: "/auth/sign-up"
+  login: "/auth/login/",
+  signUp: "/auth/sign-up/"
 })
 
 export const CREATE_CATEGORY_BOOKMARK = (categoryId: number) => {
